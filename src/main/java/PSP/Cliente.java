f7ae508c42b17cfe53b3616ab9f610203432ac17
Diego5RG-dev <dgilis@danielcastelao.org>
2025-11-06 10:06:06 +0100

    final

--- src/main/java/PSP/Cliente.java
+++ src/main/java/PSP/Cliente.java
@@ -1,29 +1,50 @@
 package PSP;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class Cliente {
 
     public static void main (String[] args){
-        InetSocketAddress dir = new InetSocketAddress("localhost", 7080);
+        InetSocketAddress dir = new InetSocketAddress("localhost", 6666);
 
         try {
             Socket socket = new Socket();
             socket.connect(dir);
-            System.out.println("conectado al servidor");
+            System.out.println("conectado al servidor escribe adios para salir");
+
+            BufferedReader teclado = new BufferedReader(
+                    new InputStreamReader(System.in)
+            );
+
+            PrintWriter escritor = new PrintWriter(socket.getOutputStream(), true);
 
             BufferedReader lector = new BufferedReader(
                     new InputStreamReader(socket.getInputStream())
             );
 
-            String mensaje = lector.readLine();
-            System.out.println("Servidor dice:" + mensaje);
+            String mensaje;
 
+            while (true) {
+                System.out.print("Mensaje ; ");
+                mensaje = teclado.readLine();
+
+                escritor.println(mensaje);
+
+                if (mensaje.equalsIgnoreCase("adios")) {
+                    break;
+                }
+
+
+                String respuestaServidor = lector.readLine();
+                if (respuestaServidor != null) {
+                    System.out.println(respuestaServidor);
+                } else {
+                    System.out.println("El servidor ha cerrado la conexión inesperadamente.");
+                    break;
+                }
+            }
             socket.close();
 
         } catch (IOException e) {
--- src/main/java/PSP/Servidor.java
+++ src/main/java/PSP/Servidor.java
@@ -1,5 +1,6 @@
 package PSP;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
@@ -9,16 +10,37 @@ import java.net.Socket;
 public class Servidor {
     public static void main (String[] args){
         try {
-            InetSocketAddress dir = new InetSocketAddress("localhost", 8080);
+            InetSocketAddress dir = new InetSocketAddress("localhost", 6666);
             ServerSocket servidor = new ServerSocket();
             servidor.bind(dir);
 
             System.out.println("esperando conexiones ");
             Socket socket = servidor.accept();
-            System.out.println("cliente conectado");
+            System.out.println("cliente conectado " + socket.getInetAddress().getHostAddress());
+
+            BufferedReader lector = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+
 
             PrintWriter escritor = new PrintWriter(socket.getOutputStream(), true);
-            escritor.println("Hola, cliente");
+
+            String mensajeCliente;
+
+            while (true) {
+                mensajeCliente = lector.readLine();
+
+                if (mensajeCliente == null || mensajeCliente.equalsIgnoreCase("adios")) {
+                    System.out.println("Cliente ha terminado la conversación o cerrado la conexión.");
+                    break;
+                }
+
+                System.out.println("Cliente dice: " + mensajeCliente);
+
+                String respuestaEco = "ECO: " + mensajeCliente;
+                escritor.println(respuestaEco);
+                System.out.println("Enviando:  " + respuestaEco);
+            }
 
             socket.close();
             servidor.close();
